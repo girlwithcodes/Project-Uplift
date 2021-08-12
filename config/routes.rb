1e2fb600c4faa730fa6faee193f3adc6408439f8
girlwithcodes <heather.mantooth0@gmail.com>
2021-08-11 20:22:31 -0500

    Defines routes for users, boards, and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: [:show, :create] do
+    resources :boards do
+      resources :posts
+    end
+  end
+
+  post '/users/login', to: 'users#login'
+  get '/users/verify', to: 'users#verify'
+  get '/posts', to: 'posts#index'
 end
