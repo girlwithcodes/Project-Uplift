5ad668034ea1c0e9d88f28402c9a937005ad3f79
girlwithcodes <heather.mantooth0@gmail.com>
2021-08-14 18:42:17 -0500

    Adds new user Register screen and functionality

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -1,5 +1,19 @@
 import api from './apiConfig';
 
+export const register = async(userInfo) => {
+  try {
+    const res = await api.post('/users', { user: userInfo });
+    const { token } = res.data
+    if(token) {
+      localStorage.setItem('authToken', token);
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+      return res.data;
+    }
+  } catch (error) {
+    throw error
+  }
+
+}
 export const login = async(userInfo) => {
   try {
     const res = await api.post('/users/login', {user: userInfo});
