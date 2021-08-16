45cb58b69f15b04f73888cd5a6dcdfd48891539e
girlwithcodes <heather.mantooth0@gmail.com>
2021-08-16 16:02:50 -0500

    Adds more styling to home page.

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -7,8 +7,18 @@ function Home() {
         <h1>Welcome to Project Uplift</h1>
       </div>
       <section className="home-page-welcome">
+        <div className="welcome-message">
+          <p>The story we tell ourselves is important.  Words have the power to shape our thoughts, our attitudes, our habits, and our lives.</p>
+          <p>At Project Uplift, you can harness that power by creating your own affirmation, celebration, blessing and wisdom statements, saving them to your boards, and sharing them with the world. Spread a little joy and positivity today!</p>
+          <p>Browse</p>
+          <p>Affirmations</p>
+          <p>Celebrations</p>
+          <p>Blessings</p>
+          <p>Wisdom</p>
+
+        </div>
         <div className="welcome-image"></div>
-        <div className="welcome-message"></div>
+
       </section>
     </div>
   )
