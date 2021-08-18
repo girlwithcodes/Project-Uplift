2e2620f4538dfd4b623925f1ef0fa8d7adfa8e82
girlwithcodes <heather.mantooth0@gmail.com>
2021-08-18 00:40:00 -0500

    Adds styling to homepage.  Creates hamburger menu and slide-out sidebar for nav links for responsiveness

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -6,14 +6,20 @@ function Home() {
       <div className="home-page-title">
         <h1>Welcome to Project Uplift</h1>
       </div>
+
       <section className="home-page-welcome">
         <div className="welcome-message">
-          <h2>The <span className="highlight-word">story</span> we tell ourselves is <span className="highlight-word">important</span>.  <p>  <span className="highlight-word">Words</span> are <span className="highlight-word">powerful</span>.</p></h2>
-          <div className="welcome-site-description">
+          <h2>The <span className="highlight-word" id="hw1">Stories</span> we tell ourselves are <span className="highlight-word" id="hw2">Important</span>  <p>  <span className="highlight-word" id="hw3">Words</span> are <span className="highlight-word" id="hw4">Powerful</span>.</p></h2>
+
+          <div className="site-des-bg-mssg">
+            <div className="site-desc-background"></div>
+            <div className="welcome-site-description">
             At Project Uplift, you can harness that power by creating, saving, and sharing your own positive messages. Spread a little happiness today!
           </div>
+
+          </div>
           <div className="welcome-options">
-            <p>Browse</p>
+            <h3>Browse</h3>
             <p>Affirmations</p>
             <p>Celebrations</p>
             <p>Blessings</p>
